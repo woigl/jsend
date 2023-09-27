fbaf6bcdf6c4c91b46d3d081b47d998d32498fc4
Stefan Walter <woigl@mac.com>
2023-09-28 01:45:22 +0700

    Add JSDoc source code comments

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,41 @@
+/** jSend success object. */
 export type JSendSuccess = {
+  /** Should always be set to "success". */
   status: "success";
+  /** Wrapper for data of any data type. No data should be set to null.  */
   data: any;
 };
 
+/** jSend fail object. */
 export type JSendFail = {
+  /** Should always be set to "fail". */
   status: "fail";
+  /** Wrapper for data of any data type. This data should specify why the rquest failed.  */
   data: any;
 };
 
+/** jSend error object. */
 export type JSendError = {
+  /** Should always be set to "error". */
   status: "error";
+  /** A meaningful, end-user-readable (or at the least log-worthy) message, explaining what went wrong. */
   message: string;
+  /** A numeric code corresponding to the error, if applicable. */
   code?: number;
+  /** A generic container for any other information about the error, i.e. the conditions that caused the error, stack traces, etc. */
   data?: any;
 };
 
+/** Union of `JSendSuccess`, `JSendFail`, and `JSendError`. */
 export type JSend = JSendSuccess | JSendFail | JSendError;
 
+/** The jSend object containing all functions. */
 const jSend = {
+  /**
+   * Creates a **successful** jSend object.
+   * @param data Wrapper for data of any data type. No data should be set to null.
+   * @returns Returns a successful jSend object.
+   */
   success: function (data: any): JSendSuccess {
     return {
       status: "success",
@@ -25,6 +43,11 @@ const jSend = {
     };
   },
 
+  /**
+   * Creates a **failed** jSend object.
+   * @param data Wrapper for data of any data type. This data should specify why the rquest failed.
+   * @returns Returns a failed jSend object.
+   */
   fail: function (data: any): JSendFail {
     return {
       status: "fail",
@@ -32,9 +55,20 @@ const jSend = {
     };
   },
 
+  /**
+   * Creates an **error** jSend object.
+   * @param message A meaningful, end-user-readable (or at the least log-worthy) message, explaining what went wrong.
+   * @param optional Additional optional information of the error.
+   * @returns
+   */
   error: function (
     message: string,
-    optional?: { code?: number; data?: any }
+    optional?: {
+      /** A numeric code corresponding to the error, if applicable. */
+      code?: number;
+      /** A generic container for any other information about the error, i.e. the conditions that caused the error, stack traces, etc. */
+      data?: any;
+    }
   ): JSendError {
     return {
       status: "error",
@@ -44,6 +78,11 @@ const jSend = {
     };
   },
 
+  /**
+   * Testing if the `ref` parameter is a valid jSend success object.
+   * @param ref The `ref` parameter to be tested.
+   * @returns True if the `ref` parameter is a valid jSend success object, otherwise false;
+   */
   isSuccess: function (ref: any): ref is JSendSuccess {
     return (
       typeof ref === "object" &&
@@ -53,6 +92,11 @@ const jSend = {
     );
   },
 
+  /**
+   * Testing if the `ref` parameter is a valid jSend failed object.
+   * @param ref The `ref` parameter to be tested.
+   * @returns True if the `ref` parameter is a valid jSend failed object, otherwise false;
+   */
   isFail: function (ref: any): ref is JSendFail {
     return (
       typeof ref === "object" &&
@@ -62,6 +106,11 @@ const jSend = {
     );
   },
 
+  /**
+   * Testing if the `ref` parameter is a valid jSend error object.
+   * @param ref The `ref` parameter to be tested.
+   * @returns True if the `ref` parameter is a valid jSend error object, otherwise false;
+   */
   isError: function (ref: any): ref is JSendError {
     return (
       typeof ref === "object" &&
@@ -72,6 +121,11 @@ const jSend = {
     );
   },
 
+  /**
+   * Testing if the `ref` parameter is one of [{@link jSend.isSuccess}, {@link jSend.isFail}, {@link jSend.isError}].
+   * @param ref The `ref` parameter to be tested.
+   * @returns True if the `ref` parameter is one of [{@link jSend.isSuccess}, {@link jSend.isFail}, {@link jSend.isError}], otherwise false.
+   */
   isValid: function (ref: any): ref is JSend {
     return this.isSuccess(ref) || this.isFail(ref) || this.isError(ref);
   },
